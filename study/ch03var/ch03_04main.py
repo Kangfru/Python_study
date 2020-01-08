0a81e66f35e27891e134ffdb267c7dbe25581759
Kangfru <kwooooook@gmail.com>
2020-01-08 12:22:49 +0900

    initial commit

--- study/ch03var/ch03_04main.py
+++ study/ch03var/ch03_04main.py
@@ -6,5 +6,16 @@ def my_func():
 gvar = 100
 
 # 메인 코드
-if __name__ == '__main__':
+# if __name__ == '__main__':
+# main 함수로만 선언을 한 경우 메인으로 인정을 못받음
+# 함수 선언 없이 처리문을 만들거나 if __name__ 선언해서 처리
+def main():
+    print("메인 함수 부분")
     my_func()
+    print("전역 변수 gvar : ", gvar)
+
+#if __name__ == '__main__':
+#   main()
+
+# main 함수 호출로 간단히 구현 가능
+main()
